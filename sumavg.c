0189ab30a1ff72311f696d2c38255e463e693034
kavicse <30829826+kavicse@users.noreply.github.com>
2018-01-28 17:01:34 +0530

    Create sumavg.c

---
+++ sumavg.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	int n,a[30],avg,sum=0,i;
+	printf("\nEnter number ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+		sum=sum+a[i];
+	}
+	avg=sum/n;
+	printf("\nAverage is %d",avg);
+}
