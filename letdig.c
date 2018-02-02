a9e3e2c7545b5a448c9e38de63f52487053f79da
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-02 13:59:17 +0530

    Create letdig.c

---
+++ letdig.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	char ch[70];
+	int k;
+	printf("\nGet string");
+	scanf("%s",&ch);
+	printf("\nGet k value");
+	scanf("%d",&k);
+	int i;
+	for(i=0;i<k;i++)
+	{
+		printf("%c",ch[i]);
+	}
+}
