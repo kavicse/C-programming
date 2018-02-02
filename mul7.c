e91e0f52d21ecc9d82f6cc7ee313957d982a10a8
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-02 14:36:25 +0530

    Create mul7.c

---
+++ mul7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int n;
+	printf("\nEnter the number");
+	scanf("%d",&n);
+	if(n%7==0)
+	{
+		printf("\nMultiple of 7");
+	}
+	else
+	{
+		
+		printf("\n Not Multiple of 7");
+	}
+}
