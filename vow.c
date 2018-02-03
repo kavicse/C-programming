25eb58c7bd4fcb6d53bc153ff7021fe25f1900cc
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-03 06:51:55 -0800

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	char ch;
+	int i,count;
+	printf("\nEnter string");
+	scanf("%s",&ch);
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+		{
+			printf("\nVowels is present");
+		}
+		else
+		{
+			printf("\nNo vowels");
+		}
+
+	
+}
