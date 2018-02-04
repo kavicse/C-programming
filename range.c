e0e4f3158c1b17f93771c85bf048ef20f0195d99
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-04 04:50:52 -0800

    Create range.c

---
+++ range.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\nEnter the number");
+scanf("%d",&n);
+	if(n<=10&&n>=1)
+	{
+		printf("\nYes between range");
+	}
+    else
+    {
+ 	printf("\nNot between range");
+	}
+	return 0;
+}
