a0983caa258f5c7d6681be8f005996f540e2a05b
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-07 06:33:43 -0800

    Create pangram.c

---
+++ pangram.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+ char s[100];
+ int i,used[26]={0},total=0;
+ printf("\n Enter the String:");
+ gets(s);
+ for(i=0;s[i]!=' ';i++)
+ {
+  if('a'<=s[i] && s[i]<='z')
+  {
+   total+=!used[s[i]-'a'];
+   used[s[i]-'a']=1;
+  }
+  else if('A'<=s[i] && s[i]<='Z')
+  {
+   total+=!used[s[i]-'A'];
+   used[s[i]-'A']=1;
+  }
+ }
+ if(total==26)
+ {
+  printf("\n The Entered String is a Pangram String.");
+ }
+ else
+ {
+  printf("\nThe Entered String is not a Pangram String.");
+ }
+ getch();
+}
