fb4775d2e47c05069415212ea5be09772f1ec5ba
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-08 16:27:34 +0530

    Create multiple13.c

---
+++ multiple13.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("\nEnter the number");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("\nMultiple by 13");
+}
+else
+{
+printf("\nNot multiple by 13");
+}
+}
