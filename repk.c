a5cf7217ccef4198a974b001ee3c15c53c759041
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-09 09:08:00 +0000

    Create repk.c

---
+++ repk.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+  int n,a[30],i,count,k;
+  printf("\nEnter number");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  printf("\nEnter k value");
+  scanf("%d",&k);
+  for(i=0;i<n;i++)
+  {
+    if(a[i]==k)
+    {
+      count++;
+    }
+  }
+  printf("%d",count);
+}
