c37cc6b71aa491e8450566fee70afa6e88d8a708
kavicse <30829826+kavicse@users.noreply.github.com>
2018-02-09 09:37:17 +0000

    Create cub.c

---
+++ cub.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  int l,b,h,vol;
+  printf("\nEnter length:");
+  scanf("%d",&l);
+  printf("\nEnter Breadth:");
+   scanf("%d",&b);
+  printf("\nEnter height:");
+   scanf("%d",&h);
+   vol=l*b*h;
+   printf("\nVolume of cuboid:%d",vol);
+}
