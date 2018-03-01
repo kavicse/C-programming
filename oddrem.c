ede6d81af6a1418f6229c15cb76c8cf335ff5279
kavicse <30829826+kavicse@users.noreply.github.com>
2018-03-01 16:30:34 +0530

    Update oddrem.c

--- oddrem.c
+++ oddrem.c
@@ -1,19 +1,24 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+void main()
 {
-  int n,rem,r;
-  printf("\nEnter the number");
-  scanf("%d",&n);
-  while(n!=0)
-  {
-    rem=n%10;
-    n=n/10;
-    {
-      if((rem%2)!=0)
-      {
-        printf("\n%d",rem);
-      }
-    }
-  }
-  
+	int n,t,rem=0,i;
+	printf("Enter the number:");
+    scanf("%d",&n);
+	while(n!=0)
+	{
+		t=n%10;
+		rem=(rem*10)+t;
+		n=n/10;
+	}
+	printf("The odd numbers are:");
+	while(rem!=0)
+	{
+		i=rem%10;
+		rem=rem/10;
+		if(i%2!=0)
+		{
+		  printf("%d  ",i);
+		}
+
+	}
 }
