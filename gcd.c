33335156379c83d9aee7dffe60f8cdfe9daa117d
kavicse <30829826+kavicse@users.noreply.github.com>
2018-03-03 16:33:57 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+  int n1,n2,d,n,r,gcd,denominator,numerator;
+  printf("\nEnter n1:");
+  scanf("%d",&n1);
+  printf("\nEnter n2:");
+  scanf("%d",&n2);
+  d=denominator;
+  n=numerator;
+  if(n1>n2)
+  {
+    n=n1;
+    d=n2;
+  }
+  else
+  {
+    n=n2;
+    d=n1;
+  }
+  r=n%d;
+  while(r!=0)
+  {
+    n=d;
+    d=r;
+    r=n%d;
+  }
+  gcd=d;
+  printf("\nGCD:%d",gcd);
+}
